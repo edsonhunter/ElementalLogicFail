2761327ebe838953292826382bb4ce63f3d5a43a
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-13 17:17:40 -0300

    Add element comparison

--- Assets/ElementLogicFail/Scripts/Domain.Interface/IElement.cs
+++ Assets/ElementLogicFail/Scripts/Domain.Interface/IElement.cs
@@ -2,6 +2,25 @@
 {
     public interface IElement
     {
+        ElementType Type { get; }
         
+        bool IsSameType(IElement element);
     }
+
+    public enum ElementType
+    {
+        Unknown,
+        Fire,
+        Water,
+        Earth,
+        Wind
+    }
+    
+    //Future ideas. Joining different elements together results in different outcomes:
+    //Fire+Water=Smoke
+    //Fire+Earth=Lava
+    //Fire+Wind=Explosion
+    //Water+Earth=Mud
+    //Water+Wind=Rain
+    //Earth+Wind=Hurricane
 }
