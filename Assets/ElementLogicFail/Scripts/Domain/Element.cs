a3b839e3099fec18bbf3d35233d1dde0110e3018
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-13 17:17:40 -0300

    Add element comparison

--- Assets/ElementLogicFail/Scripts/Domain/Element.cs
+++ Assets/ElementLogicFail/Scripts/Domain/Element.cs
@@ -1,7 +1,29 @@
-﻿namespace ElementLogicFail.Scripts.Domain
+﻿using System;
+using ElementLogicFail.Scripts.Domain.Interface;
+
+namespace ElementLogicFail.Scripts.Domain
 {
-    public class Element
+    public class Element : IElement
     {
+        public ElementType Type { get; }
+
+        private Element()
+        {
+            Type = ElementType.Unknown;
+        }
         
+        public Element(ElementType type) : this()
+        {
+            Type = type;
+        }
+        
+        public bool IsSameType(IElement element)
+        {
+            if (element == null)
+            {
+                throw new InvalidOperationException($"element object is null");
+            }
+            return element.Type == Type;
+        }
     }
 }
