831395429075b7e833cfdd44fb55333b7af012a8
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-14 01:52:18 -0300

    Move elements

---
+++ Assets/ElementLogicFail/Scripts/Systems/Wander/WanderSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace ElementLogicFail.Scripts.Systems.Wander
+{
+    public partial struct WanderSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+
+        }
+    }
+}
