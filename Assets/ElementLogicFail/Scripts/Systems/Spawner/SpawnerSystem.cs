2fb9cc79a9474ee1bb68eacd2d7fa4c35f044088
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-14 01:03:01 -0300

    Spawn fire

--- Assets/ElementLogicFail/Scripts/Systems/Spawner/SpawnerSystem.cs
+++ Assets/ElementLogicFail/Scripts/Systems/Spawner/SpawnerSystem.cs
@@ -1,5 +1,8 @@
-﻿using Unity.Burst;
+﻿using ElementLogicFail.Scripts.Components.Spawner;
+using Unity.Burst;
 using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 
 namespace ElementLogicFail.Scripts.Systems.Spawner
 {
@@ -8,13 +11,36 @@ namespace ElementLogicFail.Scripts.Systems.Spawner
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
-            
+            state.RequireForUpdate<SpawnControl>();
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {
+            float deltaTime = SystemAPI.Time.DeltaTime;
+            float multiplier = SystemAPI.GetSingleton<SpawnControl>().SpawnRateMultiplier;
+            EntityCommandBuffer entityCommandBuffer = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
 
+            foreach (var (spawner, xform) in
+                     SystemAPI.Query<RefRW<Components.Spawner.Spawner>, RefRO<LocalTransform>>())
+            {
+                Components.Spawner.Spawner spawnerRW = spawner.ValueRW;
+                spawnerRW.SpawnRate += deltaTime;
+                if (spawnerRW.SpawnRate >= math.max(0.01f, spawnerRW.SpawnRate * multiplier))
+                {
+                    spawnerRW.SpawnRate = 0f;
+                    var newInstance = entityCommandBuffer.Instantiate(spawnerRW.ElementPrefab);
+                    entityCommandBuffer.SetComponent(newInstance, new LocalTransform
+                    {
+                        Position = xform.ValueRO.Position,
+                        Rotation = xform.ValueRO.Rotation,
+                        Scale = 1f
+                    });
+                }
+                spawner.ValueRW = spawnerRW;
+            }
+            entityCommandBuffer.Playback(state.EntityManager);
+            entityCommandBuffer.Dispose();
         }
 
         [BurstCompile]
