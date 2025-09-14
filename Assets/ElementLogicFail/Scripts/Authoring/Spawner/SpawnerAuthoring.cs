af2abae088064108f9b9b62f4f2f78dcb2a1fd8f
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-14 00:21:58 -0300

    Create spawner baker

---
+++ Assets/ElementLogicFail/Scripts/Authoring/Spawner/SpawnerAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ElementLogicFail.Scripts.Authoring.Spawner
+{
+    public class SpawnerAuthoring : MonoBehaviour
+    {
+        private class SpawnerBaker : Baker<SpawnerAuthoring>
+        {
+            public override void Bake(SpawnerAuthoring authoring)
+            {
+            }
+        }
+    }
+}
