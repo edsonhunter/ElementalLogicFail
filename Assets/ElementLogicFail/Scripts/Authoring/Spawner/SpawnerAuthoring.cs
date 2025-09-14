fb613d5613d17898be4f58a0c3a0cbce534e62c6
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-14 01:13:43 -0300

    Add subScene and missing control

--- Assets/ElementLogicFail/Scripts/Authoring/Spawner/SpawnerAuthoring.cs
+++ Assets/ElementLogicFail/Scripts/Authoring/Spawner/SpawnerAuthoring.cs
@@ -1,6 +1,8 @@
 ﻿using ElementLogicFail.Scripts.Components.Element;
 using ElementLogicFail.Scripts.Components.Request;
+using ElementLogicFail.Scripts.Components.Spawner;
 using Unity.Entities;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace ElementLogicFail.Scripts.Authoring.Spawner
@@ -24,6 +26,12 @@ namespace ElementLogicFail.Scripts.Authoring.Spawner
                 Type = authoring.type,
                 SpawnRate = authoring.spawnRate,
                 ElementPrefab = prefabEntity
+            });
+            AddComponent(entity, LocalTransform.Identity);
+            
+            AddComponent(GetEntity(TransformUsageFlags.None), new SpawnControl
+            {
+                SpawnRateMultiplier = 1f
             });
 
             AddBuffer<ElementSpawnRequest>(GetEntity(TransformUsageFlags.None));
