aa36ed8682e45bb4732675426fe66abbb6ac36f5
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-14 11:43:11 -0300

    Create collisionSystem

---
+++ Assets/ElementLogicFail/Scripts/Systems/CollisionSystem.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Burst;
+using Unity.Entities;
+
+namespace ElementLogicFail.Scripts.Systems
+{
+    public partial struct CollisionSystem : ISystem
+    {
+        [BurstCompile]
+        public void OnCreate(ref SystemState state)
+        {
+            
+        }
+
+        [BurstCompile]
+        public void OnUpdate(ref SystemState state)
+        {
+
+        }
+
+        [BurstCompile]
+        public void OnDestroy(ref SystemState state)
+        {
+
+        }
+    }
+}
