ab657318c0f53ce0f6f011f024693b6699f58b4d
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-13 21:56:15 -0300

    Add DOTS structure

---
+++ Assets/ElementLogicFail/Scripts/Components/Element/ElementData.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+
+namespace ElementLogicFail.Scripts.Components
+{
+    public struct ElementData : IComponentData
+    {
+        public ElementType Type;
+    }
+
+    public enum ElementType : byte
+    {
+        Fire,
+        Water,
+        Earth,
+        Wind
+    }
+}
