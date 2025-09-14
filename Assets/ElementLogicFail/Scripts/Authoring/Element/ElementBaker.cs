ad0e36568fbd2f6a4f7629a493f2904bfb26b551
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-13 22:06:26 -0300

    Create spawner

---
+++ Assets/ElementLogicFail/Scripts/Authoring/Element/ElementBaker.cs
@@ -0,0 +1,15 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace ElementLogicFail.Scripts.Authoring.Element
+{
+    public class ElementBaker : MonoBehaviour
+    {
+        private class ElementBakerBaker : Baker<ElementBaker>
+        {
+            public override void Bake(ElementBaker authoring)
+            {
+            }
+        }
+    }
+}
---
+++ Assets/ElementLogicFail/Scripts/Components/Request/ElementSpawnRequest.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace ElementLogicFail.Scripts.Components.Request
+{
+    public struct ElementSpawnRequest : IComponentData
+    {
+        
+    }
+}
