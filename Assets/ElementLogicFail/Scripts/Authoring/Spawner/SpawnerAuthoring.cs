36b8f5d5fc0bfb1c25dc14aa989b4ba95d12ece4
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-14 00:49:15 -0300

    Spawn element

--- Assets/ElementLogicFail/Scripts/Authoring/Spawner/SpawnerAuthoring.cs
+++ Assets/ElementLogicFail/Scripts/Authoring/Spawner/SpawnerAuthoring.cs
@@ -1,15 +1,32 @@
-﻿using Unity.Entities;
+﻿using ElementLogicFail.Scripts.Components.Element;
+using ElementLogicFail.Scripts.Components.Request;
+using Unity.Entities;
 using UnityEngine;
 
 namespace ElementLogicFail.Scripts.Authoring.Spawner
 {
     public class SpawnerAuthoring : MonoBehaviour
     {
-        private class SpawnerBaker : Baker<SpawnerAuthoring>
+        public ElementType type;
+        public GameObject prefab;
+        public float spawnRate;
+    }
+    
+    public class SpawnerBaker : Baker<SpawnerAuthoring>
+    {
+        public override void Bake(SpawnerAuthoring authoring)
         {
-            public override void Bake(SpawnerAuthoring authoring)
+            var entity = GetEntity(TransformUsageFlags.Dynamic);
+            var prefabEntity = GetEntity(authoring.prefab, TransformUsageFlags.Dynamic);
+
+            AddComponent(entity, new Components.Spawner.Spawner
             {
-            }
+                Type = authoring.type,
+                SpawnRate = authoring.spawnRate,
+                ElementPrefab = prefabEntity
+            });
+
+            AddBuffer<ElementSpawnRequest>(GetEntity(TransformUsageFlags.None));
         }
     }
 }
