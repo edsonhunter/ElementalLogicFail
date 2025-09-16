2210f50fa64e4f6d125b64ce4234bad9cd834a3a
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-15 21:07:45 -0300

    Update custom unity physic to version 1.3.14

--- Assets/Unity Physics/1.3.14/Custom Physics Authoring/Unity.Physics.Custom/Bodies/PhysicsShapeAuthoring.cs
+++ Assets/Unity Physics/1.3.14/Custom Physics Authoring/Unity.Physics.Custom/Bodies/PhysicsShapeAuthoring.cs
@@ -185,7 +185,7 @@ namespace Unity.Physics.Authoring
             height = props.Height;
         }
 
-        internal CapsuleGeometryAuthoring GetCapsuleProperties()
+        public CapsuleGeometryAuthoring GetCapsuleProperties()
         {
             GetCylindricalProperties(
                 m_Capsule, out var center, out var height, out var radius, out var orientationEuler, m_ShapeType != ShapeType.Capsule
@@ -610,7 +610,7 @@ namespace Unity.Physics.Authoring
             SyncSphereProperties();
         }
 
-        internal void SetCapsule(CapsuleGeometryAuthoring geometry)
+        public void SetCapsule(CapsuleGeometryAuthoring geometry)
         {
             m_ShapeType = ShapeType.Capsule;
             m_PrimitiveCenter = geometry.Center;
@@ -802,22 +802,23 @@ namespace Unity.Physics.Authoring
         internal unsafe void BakePoints(NativeArray<float3> points)
         {
             var localToShapeQuantized = GetLocalToShapeMatrix();
-            using (var aabb = new NativeArray<Aabb>(1, Allocator.TempJob))
-            {
-                new PhysicsShapeExtensions.GetAabbJob { Points = points, Aabb = aabb }.Run();
-                HashableShapeInputs.GetQuantizedTransformations(localToShapeQuantized, aabb[0], out localToShapeQuantized);
-            }
-            using (var bakedPoints = new NativeArray<float3>(points.Length, Allocator.TempJob, NativeArrayOptions.UninitializedMemory))
-            {
-                new BakePointsJob
-                {
-                    Points = points,
-                    LocalToShape = localToShapeQuantized,
-                    Output = bakedPoints
-                }.Schedule(points.Length, 16).Complete();
+            var aabb = new Aabb { Min = float.MaxValue, Max = float.MinValue };
+            foreach (var p in points)
+                aabb.Include(p);
 
-                UnsafeUtility.MemCpy(points.GetUnsafePtr(), bakedPoints.GetUnsafePtr(), points.Length * UnsafeUtility.SizeOf<float3>());
-            }
+            HashableShapeInputs.GetQuantizedTransformations(localToShapeQuantized, aabb, out localToShapeQuantized);
+            using var bakedPoints = new NativeArray<float3>(points.Length, Allocator.TempJob,
+                NativeArrayOptions.UninitializedMemory);
+
+            new BakePointsJob
+            {
+                Points = points,
+                LocalToShape = localToShapeQuantized,
+                Output = bakedPoints
+            }.Schedule(points.Length, 16).Complete();
+
+            UnsafeUtility.MemCpy(points.GetUnsafePtr(), bakedPoints.GetUnsafePtr(),
+                points.Length * UnsafeUtility.SizeOf<float3>());
         }
 
         [BurstCompile]
--- Assets/Unity Physics/1.3.14/Custom Physics Authoring/Unity.Physics.Custom/Motors/LinearVelocityMotor.cs
+++ Assets/Unity Physics/1.3.14/Custom Physics Authoring/Unity.Physics.Custom/Motors/LinearVelocityMotor.cs
@@ -1,27 +1,30 @@
+using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 
 namespace Unity.Physics.Authoring
 {
-    public class PositionMotor : BaseJoint
+    public class LinearVelocityMotor : BaseJoint
     {
-        [Tooltip("An offset from the center of the body with the motor, representing the anchor point of translation.")]
+        [Tooltip("An offset from the center of the body with the motor (bodyA), representing the anchor point of translation.")]
         public float3 AnchorPosition;
         [Tooltip("The direction of the motor, relative to the orientation of the Connected Body (bodyB). Value will be normalized")]
         public float3 DirectionOfMovement;
-        [Tooltip("Motor will drive this length away from the anchor position of bodyA.")]
-        public float TargetDistance;
+        [Tooltip("Motor will drive at this speed from the initial position of bodyA, along the Direction of Movement, in m/s.")]
+        public float TargetSpeed;
         [Tooltip("The magnitude of the maximum impulse the motor can exert in a single step. Applies only to the motor constraint.")]
         public float MaxImpulseAppliedByMotor = math.INFINITY;
+        [Tooltip("A ratio describing how quickly a motor will arrive at the target. A value of 0 will oscillate about a solution indefinitely, while a value of 1 is critically damped. Default value is 2530.126 which describes a stiff spring")]
+        public float DampingRatio = Constraint.DefaultDampingRatio;
 
         private float3 PerpendicularAxisLocal;
         private float3 PositionInConnectedEntity;
         private float3 AxisInConnectedEntity;
         private float3 PerpendicularAxisInConnectedEntity;
 
-        class PositionMotorBaker : JointBaker<PositionMotor>
+        class LinearVelocityMotorBaker : JointBaker<LinearVelocityMotor>
         {
-            public override void Bake(PositionMotor authoring)
+            public override void Bake(LinearVelocityMotor authoring)
             {
                 float3 axisInB = math.normalize(authoring.DirectionOfMovement);
 
@@ -33,14 +36,14 @@ namespace Unity.Physics.Authoring
                 authoring.AxisInConnectedEntity = axisInB; //motor axis in Connected Entity space
 
                 // Always calculate the perpendicular axes
-                Math.CalculatePerpendicularNormalized(axisInA, out var perpendicularLocal, out _);
-                authoring.PerpendicularAxisInConnectedEntity = math.mul(bFromA.rot, perpendicularLocal); //perp motor axis in Connected Entity space
+                Math.CalculatePerpendicularNormalized(axisInA, out var perpendicularAxisLocal, out _);
+                authoring.PerpendicularAxisInConnectedEntity = math.mul(bFromA.rot, perpendicularAxisLocal); //perp motor axis in Connected Entity space
 
-                var joint = PhysicsJoint.CreatePositionMotor(
+                var joint = PhysicsJoint.CreateLinearVelocityMotor(
                     new BodyFrame
                     {
                         Axis = axisInA,
-                        PerpendicularAxis = perpendicularLocal,
+                        PerpendicularAxis = perpendicularAxisLocal,
                         Position = authoring.AnchorPosition
                     },
                     new BodyFrame
@@ -49,12 +52,14 @@ namespace Unity.Physics.Authoring
                         PerpendicularAxis = authoring.PerpendicularAxisInConnectedEntity,
                         Position = authoring.PositionInConnectedEntity
                     },
-                    authoring.TargetDistance,
-                    authoring.MaxImpulseAppliedByMotor
+                    authoring.TargetSpeed,
+                    authoring.MaxImpulseAppliedByMotor,
+
+                    Constraint.DefaultSpringFrequency,
+                    authoring.DampingRatio
                 );
 
                 joint.SetImpulseEventThresholdAllConstraints(authoring.MaxImpulse);
-
                 var constraintBodyPair = GetConstrainedBodyPair(authoring);
 
                 uint worldIndex = GetWorldIndexFromBaseJoint(authoring);
