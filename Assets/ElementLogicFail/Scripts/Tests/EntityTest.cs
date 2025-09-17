552d958bdd1d8af2e7776c282c611280df9d35b9
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-16 21:38:29 -0300

    Add all dependencies and references in asmdefs

--- Assets/ElementLogicFail/Scripts/Tests/EntityTest.cs
+++ Assets/ElementLogicFail/Scripts/Tests/EntityTest.cs
@@ -1,7 +1,25 @@
-﻿namespace ElementLogicFail.Scripts.Tests
+﻿using ElementLogicFail.Scripts.Components.Element;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace ElementLogicFail.Scripts.Tests
 {
-    public class EntityTest
+    public static class EntityTest
     {
-        
+        public static Entity CreateElement(EntityManager manager, ElementType type, float speed, int cooldown)
+        {
+            var entity = manager.CreateEntity();
+            manager.SetComponentData(entity, new ElementData()
+            {
+                Type= type,
+                Speed = speed,
+                Cooldown = cooldown,
+                Target = float3.zero,
+                RandomSeed = (uint)UnityEngine.Random.Range(1, int.MaxValue)
+            });
+            manager.SetComponentData(entity, LocalTransform.FromPosition(new float3(0, 0, 0)));
+            return entity;
+        }
     }
 }
