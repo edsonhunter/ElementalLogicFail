75dfdafbe7dc8dddc588f2d20ed8f15c1b80c017
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-19 15:19:58 -0300

    Add small game flow to the scenes

--- Assets/ElementLogicFail/Scripts/Scenes/MainMenuSceneController.cs
+++ Assets/ElementLogicFail/Scripts/Scenes/MainMenuSceneController.cs
@@ -1,13 +1,29 @@
-﻿using ElementLogicFail.Scripts.Scenes.Interface;
+﻿using ElementLogicFail.Scripts.Manager.Interface;
+using ElementLogicFail.Scripts.Scenes.Interface;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ElementLogicFail.Scripts.Scenes
 {
     public class MainMenuSceneController : BaseScene<MainMenuData>
     {
+        [field: SerializeField] private Button playGameButton;
+        [field: SerializeField] private Button configButton;
         protected override void Loaded()
         {
             Debug.Log("Main Menu Load Complete");
+            playGameButton.onClick.AddListener(PlayGame);
+            configButton.onClick.AddListener(OpenConfig);
+        }
+
+        private void PlayGame()
+        {
+            GetManager<ISceneManager>().LoadScene(new GameplayData());
+        }
+
+        private void OpenConfig()
+        {
+            GetManager<ISceneManager>().LoadOverlayScene(new ConfigData());
         }
     }
     
