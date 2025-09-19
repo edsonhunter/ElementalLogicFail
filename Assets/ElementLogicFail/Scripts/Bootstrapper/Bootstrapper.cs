73f3fdd93f0a03d5b68a907b52261724d48d5867
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-19 11:11:42 -0300

    Prepare bootstrapper

---
+++ Assets/ElementLogicFail/Scripts/Bootstrapper/Bootstrapper.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace ElementLogicFail.Scripts.Bootstrapper
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        private void Start()
+        {
+            //Create applicationPrefab
+            //Initialize applicationPrefab
+            //Initialize Managers
+            //Initialize Services
+            //Start App
+        }
+    }
+}
