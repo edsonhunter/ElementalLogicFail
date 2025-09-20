08e59bfed2fd30fd0dfbeaadee4232ffb12d23aa
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-19 22:58:22 -0300

    Create a spawnerController for the spawner rate

---
+++ Assets/ElementLogicFail/Scripts/Components/Element/ElementType.cs
@@ -0,0 +1,11 @@
+﻿namespace ElementLogicFail.Scripts.Components.Element
+{
+    public enum ElementType
+    {
+        Unknown = 0,
+        Fire = 1,
+        Water = 2,
+        Earth = 3,
+        Wind = 4
+    }
+}
---
+++ Assets/ElementLogicFail/Scripts/Components/Request/SpawnerRateChangeRequest.cs
@@ -0,0 +1,11 @@
+﻿using ElementLogicFail.Scripts.Components.Element;
+using Unity.Entities;
+
+namespace ElementLogicFail.Scripts.Components.Request
+{
+    public struct SpawnerRateChangeRequest : IBufferElementData
+    {
+        public ElementType Type;
+        public float NewRate;
+    }
+}
