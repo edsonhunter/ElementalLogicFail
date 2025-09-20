924e6679f6de2d8c2f5b5279beef0030dc05d774
Edson N. Ferrao <nicferrao@gmail.com>
2025-09-19 22:08:10 -0300

    Create cooldown system tests

---
+++ Assets/ElementLogicFail/Scripts/Tests/Systems/CooldownSystemTest.cs
@@ -0,0 +1,39 @@
+﻿using ElementLogicFail.Scripts.Components.Element;
+using ElementLogicFail.Scripts.Systems.Collision;
+using ElementLogicFail.Scripts.Tests.Editor;
+using NUnit.Framework;
+using Unity.Entities;
+
+namespace ElementLogicFail.Scripts.Tests.Systems
+{
+    [TestFixture]
+    public class CooldownSystemTest : ECSTestFixture
+    {
+        [Test]
+        public void CooldownSystem_ReducesCooldownOverTime()
+        {
+            var entity = EntityManager.CreateEntity(typeof(ElementData));
+            float initialCooldown = 5.0f;
+            EntityManager.SetComponentData(entity, new ElementData { Cooldown = initialCooldown });
+            
+            World.GetOrCreateSystem<CooldownSystem>().Update(World.Unmanaged);
+            
+            var newCooldown = EntityManager.GetComponentData<ElementData>(entity).Cooldown;
+            Assert.Less(newCooldown, initialCooldown);
+        }
+
+        [Test]
+        public void CooldownSystem_DoesNotGoBelowZero()
+        {
+            var entity = EntityManager.CreateEntity(typeof(ElementData));
+            EntityManager.SetComponentData(entity, new ElementData { Cooldown = 0.1f });
+
+            var system = World.GetOrCreateSystem<CooldownSystem>();
+            system.Update(World.Unmanaged);
+            system.Update(World.Unmanaged);
+            
+            var newCooldown = EntityManager.GetComponentData<ElementData>(entity).Cooldown;
+            Assert.AreEqual(0f, newCooldown);
+        }
+    }
+}
